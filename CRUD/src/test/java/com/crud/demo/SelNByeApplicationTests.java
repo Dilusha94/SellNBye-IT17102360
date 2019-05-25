0378969074c67682e71b906ad68aa921abce671f
Dilusha Madhushan <dmadhushan61@gmail.com>
2019-05-25 23:01:40 +0530

    some changes of app

--- CRUD/src/test/java/com/crud/demo/SelNByeApplicationTests.java
+++ CRUD/src/test/java/com/crud/demo/SelNByeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CrudApplicationTests {
+public class SelNByeApplicationTests {
 
 	@Test
 	public void contextLoads() {
